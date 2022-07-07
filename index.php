9ae02e274d5a6f025f1f13f45a3fdcb8e0e87785
chokoladis <igor2011__igor@mail.ru>
2022-07-07 20:15:31 +0400

    1hour

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Чумаков Игорь">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>SEO Простор</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="logo d-flex flex-column">
+                    <img src="/img/logo.png" alt="">
+                    <p>Завод светопрозрачных конструкций</p>
+                </div>
+                <div class="d-none d-md-flex call_contact_center">
+                    <div class="d-flex flex-column">
+                        <a href="tel:+78462125253" class="phone">8 (846) 212-52-53</a>
+                        <p>Контакт-центр Режим работы 24/7</p>
+                        <p>Звонок бесплатный</p>
+                    </div>
+                    <div class="call_center_img">
+                        <img src="/img/call_center.png" alt="">
+                    </div>
+                </div>
+                <div class="d-flex d-md-none icon_phone">
+                    <img src="/img/phone.png" alt="">
+                </div>
+            </div>
+            <hr>
+
+        </div>
+    </header>
+</body>
+</html>
